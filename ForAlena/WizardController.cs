e96843d7b0bc7e662e339207e33b036dce6c15a7
asher.rulez <asher.rulez@gmail.com>
2019-04-04 22:53:16 +0300

    Add project files.

---
+++ ForAlena/WizardController.cs
@@ -0,0 +1,24 @@
+﻿using MVVMC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForAlena
+{
+    public class WizardController : Controller
+    {
+        private FormModel _model;
+
+        public override void Initial()
+        {
+            StartPage();
+        }
+
+        private void StartPage()
+        {
+            ExecuteNavigation();
+        }
+    }
+}
---
+++ ForAlenaDAL/ApplicationData/AppStrings.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForAlenaDAL.ApplicationData
+{
+    public class AppStrings
+    {
+        public string WindowTitle { get; set; }
+
+        public string Common_Btn_Save_Title { get; set; }
+
+        public string Common_Btn_Clear_Title { get; set; }
+
+        public string StartPage_Title { get; set; }
+
+        public string StartPage_Description { get; set; }
+
+        public string StartPage_Btn_Open_Title { get; set; }
+
+        public string StartPage_Btn_New_Title { get; set; }
+
+        public string SecondPage_Title { get; set; }
+
+        public string SecondPage_Description { get; set; }
+
+        public string SecondPage_ProcedureNumberLabel { get; set; }
+
+        public string SecondPage_UserNameLabel { get; set; }
+
+        public string SecondPage_CB_Option1Label { get; set; }
+
+        public string SecondPage_CB_Option2Label { get; set; }
+
+        public string SecondPage_CB_Option3Label { get; set; }
+
+        public string ThirdPage_Title { get; set; }
+
+        public string ThirdPage_Description { get; set; }
+
+        public string ThirdPage_Combo_Label { get; set; }
+
+        public string ForthPage_Title { get; set; }
+
+        public string ForthPage_Description { get; set; }
+
+        public string ForthPage_CB_1_Label { get; set; }
+
+        public string ForthPage_CB_2_Label { get; set; }
+
+        public string ForthPage_CB_3_Label { get; set; }
+
+        public string ForthPage_CB_4_Label { get; set; }
+
+        public string ForthPage_AdditionalExplanation_Label { get; set; }
+
+        public string FifthPage_Title { get; set; }
+
+        public string FifthPage_Description { get; set; }
+
+        public string FifthPage_Btn_AddPhoto_Title { get; set; }
+
+        public string FifthPage_PhotoName_Label { get; set; }
+
+        public string FifthPage_Btn_Print_Title { get; set; }
+    }
+}
